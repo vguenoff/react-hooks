444a275b7066276b47a3b24c4302eb0f8a0dfd54
v1 <v1@v1s-MacBook-Air.local>
2019-08-22 16:50:49 +0300

    use State hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
 const App = () => {
+    const [name, setName] = useState('Heloooo');
+
     return (
         <div className="main-wrapper">
             <h1>Level Up Dishes</h1>
+            <h3>{name}</h3>
+            <input
+                type="text"
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
         </div>
     );
 };
